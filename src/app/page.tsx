881b6c6d911d8b3a23a7a310e5a023bd58d65675
shemil01 <shemilmk1033@gmail.com>
2024-10-28 15:29:07 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
-import Link from "next/link";
+import WeatherForm from "@/components/weatherForm";
+
 
 
 export default function Home() {
   return (
     <div className="text-center">
-    <h1 className="text-3xl font-bold mb-4">Weather App</h1>
-    <Link href="/weather">
-      <button className="bg-blue-500 text-white p-2">Check Weather</button>
-    </Link>
+    <WeatherForm />
   </div>
         
   );
