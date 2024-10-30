b7f00d662003afacef5fcdcc1cc3a533b2e7bae3
shemil01 <shemilmk1033@gmail.com>
2024-10-30 12:00:45 +0530

    updated

--- src/utils/fetchWeather.ts
+++ src/utils/fetchWeather.ts
@@ -1,6 +1,6 @@
 export async function fetchWeather(city:string){
     const apiKey = 'd89717a98767454b92961801242810'
-    const apiUrl = `http://api.weatherapi.com/v1/current.json`;
+    const apiUrl = `https://api.weatherapi.com/v1/current.json`;
     if (!apiKey) throw new Error("API key is missing");
 
     const response = await fetch(`${apiUrl}?key=${apiKey}&q=${city}`);
